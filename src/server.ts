f5b83936baeb118a3d7ea76ec5281099e883d9a0
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-02-17 00:52:46 +0000

    Refactor server initialization to streamline WhatsAppBot and SocketHandler integration

--- src/server.ts
+++ src/server.ts
@@ -7,25 +7,19 @@ import SocketHandler from './modules/socket/socketHandler';
 const app = express();
 const { server, io } = socketIO(app);
 
-// Inicialize o WhatsAppBot primeiro
-const bot = new WhatsAppBot(null); // Inicialize o bot com um valor temporário
+const bot = new WhatsAppBot(null);
 
-// Inicialize o SocketHandler
-const socketHandler = new SocketHandler(io, bot); // Passar a instância do SocketHandler ao bot
+const socketHandler = new SocketHandler(io, bot);
 
-// Agora atribua o socketHandler ao bot
-bot.socketHandler = socketHandler; // Atualiza a instância do bot para incluir o SocketHandler
+bot.socketHandler = socketHandler;
 
-// No WhatsAppBot, remova a inicialização de socketHandler do construtor
-// e mova a lógica de inicialização do cliente para um método separado
-
-// Inicie o servidor
 app.use('/health', (_req, res) => {
     const healthStatus = bot.checkConnection();
     res.json(healthStatus);
 })
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
