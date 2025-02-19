a1b33813b9cd6301230924772b00b6258da32b5a
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-02-19 18:36:14 +0000

    Add error handling and logging for health check route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,31 +3,37 @@ import { bot, io } from '../server';
 import apiRouter from './api';
 import requestLogger from '../middleware/requestLogger';
 import errorHandler from '../middleware/errorHandler';
+import logger from '../config/logger';
 
 const router = Router();
 
-router.use(requestLogger)
+router.use(requestLogger);
 router.get('/health', (_req, res) => {
+  try {
     const whatsappConnection = bot.checkConnection();
     const socket = io.of('/');
-    let socketConnection = ''
+    let socketConnection = '';
     socket.emit('ping', {}, (response: string) => {
-        if (response === 'pong') {
-            socketConnection = 'OK'
-        } else {
-            socketConnection = 'Socket.IO connection error'
-        }
-    })
+      if (response === 'pong') {
+        socketConnection = 'OK';
+      } else {
+        socketConnection = 'Socket.IO connection error';
+      }
+    });
     if (whatsappConnection.error) {
-        res.status(500).json({ error: whatsappConnection.error});
+      res.status(500).json({ error: whatsappConnection.error });
     }
     res.json({
       whatsappConnection: 'OK',
-      socketConnection
-    })
+      socketConnection,
+    });
+  } catch (error: any) {
+    logger.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.use(apiRouter);
 
-router.use(errorHandler)
+router.use(errorHandler);
 export default router;
