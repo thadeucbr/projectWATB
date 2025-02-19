b5a2fd06baaace70e2ab119661d9b60068555604
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-02-19 18:33:20 +0000

    Replace console log with Winston logger for server startup message

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import socketIO from './modules/socket/socketIO';
 import WhatsAppBot from './modules/whatsapp/OpenWA';
 import SocketHandler from './modules/socket/socketHandler';
 import router from './routes';
+import logger from './config/logger';
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,7 +20,7 @@ bot.socketHandler = socketHandler;
 app.use(router);
 
 server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    logger.info(`Servidor rodando na porta ${PORT}`);
 });
 
 export { bot, io }
