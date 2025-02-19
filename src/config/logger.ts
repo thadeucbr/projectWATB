f1b5ea634b59683a423a2743b8fd6c74d18db952
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-02-19 18:48:15 +0000

    Refactor logger configuration to include separate warning log file and improve error handling

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,52 +1,49 @@
-// src/config/logger.ts
-
 import { createLogger, format, transports } from 'winston';
 import path from 'path';
 
-// Desestruturação dos formatos do Winston
 const { combine, timestamp, printf, errors } = format;
 
-// Formato personalizado para os logs
 const logFormat = printf(({ level, message, timestamp, stack }) => {
   return `${timestamp} [${level.toUpperCase()}]: ${stack || message}`;
 });
 
-// Criação da instância do logger
 const logger = createLogger({
-  level: 'info', // Define o nível mínimo de log
+  level: 'info',
   format: combine(
     timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    errors({ stack: true }), // Captura o stack trace para objetos Error
+    errors({ stack: true }),
     logFormat
   ),
   transports: [
-    // Transporte para logs de erro
     new transports.File({
       filename: path.resolve(__dirname, '../../logs/error.log'),
-      level: 'error', // Apenas logs de erro
+      level: 'error',
       handleExceptions: true,
-      maxsize: 5242880, // 5MB
+      maxsize: 52428800,
       maxFiles: 5,
     }),
-    // Transporte para logs combinados (todos os níveis)
+    new transports.File({
+      filename: path.resolve(__dirname, '../../logs/warning.log'),
+      level: 'warning',
+      handleExceptions: true,
+      maxsize: 52428800,
+      maxFiles: 5,
+    }),
+
     new transports.File({
       filename: path.resolve(__dirname, '../../logs/combined.log'),
       handleExceptions: true,
-      maxsize: 5242880, // 5MB
+      maxsize: 52428800,
       maxFiles: 5,
     }),
   ],
-  exitOnError: false, // Não finalizar o processo em exceções não tratadas
+  exitOnError: false,
 });
 
-// Se estivermos em desenvolvimento, também registrar no console
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new transports.Console({
-      format: combine(
-        format.colorize(),
-        logFormat
-      ),
+      format: combine(format.colorize(), logFormat),
     })
   );
 }
