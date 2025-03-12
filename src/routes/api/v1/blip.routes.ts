1a6f9717df28d691b885e0daf423eba01c64cbd3
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-03-12 14:48:12 +0000

    Refactor Blip routes to use appropriate HTTP methods for context management

--- src/routes/api/v1/blip.routes.ts
+++ src/routes/api/v1/blip.routes.ts
@@ -3,17 +3,17 @@ import BlipController from '../../../controller/blip/Blip.controller';
 
 const blipRouterV1 = Router();
 
-blipRouterV1.post('/blip/delete-context', async (req, res) => {
+blipRouterV1.delete('/blip/context', async (req, res) => {
   const controller = new BlipController(req, res);
   await controller.deleteContext();
 });
 
-blipRouterV1.post('/blip/get-context', async (req, res) => {
+blipRouterV1.get('/blip/context', async (req, res) => {
   const controller = new BlipController(req, res);
   await controller.getContext();
 });
 
-blipRouterV1.post('/blip/reset-context', async (req, res) => {
+blipRouterV1.put('/blip/context/reset', async (req, res) => {
   const controller = new BlipController(req, res);
   await controller.resetContext();
 });
