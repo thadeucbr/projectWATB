5d0670f5aab796f01364f7183c7eace2d351e94b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-03-12 14:48:19 +0000

    Add optional router property to context DTOs for enhanced flexibility

--- src/controller/blip/Blip.dto.ts
+++ src/controller/blip/Blip.dto.ts
@@ -6,14 +6,17 @@ export interface DeleteContextDTO {
   userId: string;
   varName: string;
   authorize: string;
+  router?: string;
 }
 
 export interface GetContextDTO {
   userId: string;
   authorize: string;
+  router?: string;
 }
 
 export interface ResetContextDTO {
   phone: string;
   authorize: string;
+  router?: string;
 }
