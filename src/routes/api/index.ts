79f593899171e359f380d2553bd0326b279f95b3
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-03-12 13:39:19 +0000

    Refactor API routing structure to consolidate versioning under a single router

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import phonesRouterV1 from './v1/phones.routes';
-import aiRouterV1 from './v1/ai.routes';
-import whatsappRouterV1 from './v1/whatsapp.routes';
-import blipRouterV1 from './v1/blip.routes';
+import apiRouterV1 from './v1';
 
 const apiRouter = Router();
 
-apiRouter.use('/api/v1', phonesRouterV1);
-apiRouter.use('/api/v1', aiRouterV1);
-apiRouter.use('/api/v1', whatsappRouterV1);
-apiRouter.use('/api/v1', blipRouterV1);
+apiRouter.use('/api', apiRouterV1);
 
 export default apiRouter;
---
+++ src/routes/api/v1/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import phonesRouterV1 from './phones.routes';
+import aiRouterV1 from './ai.routes';
+import whatsappRouterV1 from './whatsapp.routes';
+import blipRouterV1 from './blip.routes';
+
+const apiRouterV1 = Router();
+
+apiRouterV1.use('/v1', phonesRouterV1);
+apiRouterV1.use('/v1', aiRouterV1);
+apiRouterV1.use('/v1', whatsappRouterV1);
+apiRouterV1.use('/v1', blipRouterV1);
+
+export default apiRouterV1;
